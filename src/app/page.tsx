6a93dabb355596200f50e8b21d34e8bca41ccc4b
WellyaKardika <wellyakardika13@gmail.com>
2026-01-30 15:16:06 +0800

    add fade-in on floating-dock

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -646,7 +646,7 @@ export default function Home() {
           </section>
 
           {/* FLOATING DOCK INTEGRATION */}
-          <div className="fixed bottom-6 left-0 right-0 z-[100] flex justify-center pointer-events-none px-4">
+          <div className="fixed bottom-6 left-0 right-0 z-[100] flex justify-center pointer-events-none px-4 animate-fade-in-delayed">
             <div className="pointer-events-auto">
               <FloatingDock items={navItems} />
             </div>
